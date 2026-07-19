bbbb3e50d73654ec4b72ab6aed6d4c8d7ac0834f
Terence Katiba <katibaterence2@gmail.com>
2026-07-19 09:42:44 +0300

    User input capturing

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const form = document.querySelector("#search-form");
-
 const input = document.querySelector("#word-input");
-
 const result = document.querySelector("#result");
 
 form.addEventListener("submit", function(event){
-
 event.preventDefault();
+});
 
+form.addEventListener("submit",(event)=>{
+event.preventDefault();
+const word=input.value;
+console.log(word);
 });
