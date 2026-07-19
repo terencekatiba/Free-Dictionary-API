19518b17656399fff3a2dcba5dff4d4083bf3cf1
Terence Katiba <katibaterence2@gmail.com>
2026-07-19 09:39:34 +0300

    Event listner

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const form = document.querySelector("#search-form");
 const input = document.querySelector("#word-input");
 
 const result = document.querySelector("#result");
+
+form.addEventListener("submit", function(event){
+
+event.preventDefault();
+
+});
